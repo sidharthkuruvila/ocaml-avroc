7dfdb3b922c3cf77385496a61d1141eab312e186
Sidharth Kuruvila <sidharth.kuruvila@flipkart.com>
2015-04-19 06:59:31 +0530

    Changed indentation to spaces

--- src/value.ml
+++ src/value.ml
@@ -4,44 +4,44 @@ open Foreign
 type avro_value_t = unit
 let avro_value_t : avro_value_t typ = void
 
-					
+                    
 let avro_value_incref =
   foreign "avro_value_incref" (ptr avro_value_t @-> returning void)
 
 let avro_value_decref =
   foreign "avro_value_incref" (ptr avro_value_t @-> returning void)
-	  
+      
 let avro_value_copy_ref =
   foreign "avro_value_copy_ref"
-	  (ptr avro_value_t @-> ptr avro_value_t @-> returning void)
+      (ptr avro_value_t @-> ptr avro_value_t @-> returning void)
 
 let avro_value_move_ref =
   foreign "avro_value_move_ref"
-	  (ptr avro_value_t @-> ptr avro_value_t @-> returning void)
+      (ptr avro_value_t @-> ptr avro_value_t @-> returning void)
 
 let avro_value_equal =
   foreign "avro_value_equal"
-	  (ptr avro_value_t @-> ptr avro_value_t @-> returning int)
+      (ptr avro_value_t @-> ptr avro_value_t @-> returning int)
 
 let avro_value_equal_fast =
   foreign "avro_value_equal_fast"
-	  (ptr avro_value_t @-> ptr avro_value_t @-> returning int)
+      (ptr avro_value_t @-> ptr avro_value_t @-> returning int)
 
 let avro_value_cmp =
   foreign "avro_value_cmp"
-	  (ptr avro_value_t @-> ptr avro_value_t @-> returning int)
+      (ptr avro_value_t @-> ptr avro_value_t @-> returning int)
 
 let avro_value_cmp_fast =
   foreign "avro_value_cmp_fast"
-	  (ptr avro_value_t @-> ptr avro_value_t @-> returning int)
-	  
+      (ptr avro_value_t @-> ptr avro_value_t @-> returning int)
+      
 let avro_value_copy =
   foreign "avro_value_copy"
-	  (ptr avro_value_t @-> ptr avro_value_t @-> returning int)
+      (ptr avro_value_t @-> ptr avro_value_t @-> returning int)
 
 let avro_value_copy_fast =
   foreign "avro_value_copy_fast"
-	  (ptr avro_value_t @-> ptr avro_value_t @-> returning int)
+      (ptr avro_value_t @-> ptr avro_value_t @-> returning int)
 
 let avro_value_hash =
   foreign "avro_value_hash" 
