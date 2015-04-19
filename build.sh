658dc74835b7c944b733bb631a66a825625176f6
Sidharth Kuruvila <sidharth.kuruvila@flipkart.com>
2015-04-19 21:10:30 +0530

    Changed corebuild to ocamlbuild

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 CURRENT_DIR=`pwd`
 LINKER_FLAGS=-cclib,-l,-cclib,avro.22.0.0,-cclib,-L,-cclib,${CURRENT_DIR}/lib
-corebuild -r -I src -lflags $LINKER_FLAGS -pkg ctypes.foreign examples/quickstop.native
+ocamlbuild -r -I src -lflags $LINKER_FLAGS -pkg ctypes.foreign examples/quickstop.native
